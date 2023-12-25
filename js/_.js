52d5a1dc72c139be872600b808f0abbb2c677b7d
byonghwan_son <sonstein@daum.net>
2023-12-25 18:04:28 +0900

    functional 코딩

--- js/_.js
+++ js/_.js
@@ -17,26 +17,25 @@ var _get = _curryr(function (obj, key) {
 
 function _filter(list, predi) {
   // closure 클로저
-    var new_list = [];
-    _each(list, function (val) {
-      if (predi(val)) new_list.push(val);
-    });
-    return new_list;
-  }
+  var new_list = [];
+  _each(list, function (val) {
+    if (predi(val)) new_list.push(val);
+  });
+  return new_list;
+}
+
+function _map(list, mapper) {
+  // closure 클로저
+  var new_list = [];
+  _each(list, function (val) {
+    new_list.push(mapper(val));
+  });
+  return new_list;
+}
   
-  function _map(list, mapper) {
-    // closure 클로저
-    var new_list = [];
-    _each(list, function (val) {
-      new_list.push(mapper(val));
-    });
-    return new_list;
+function _each(list, iter) {
+  for (var i = 0; i < list.length; i++) {
+    iter(list[i]);
   }
-  
-  function _each(list, iter) {
-    for (var i = 0; i < list.length; i++) {
-      iter(list[i]);
-    }
-    return list;
-  }
-  
+  return list;
+}
